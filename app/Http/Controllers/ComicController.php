ec7df3d5478fffd25f27c4cc18ac569deb9a2222
jacoposergio <djago32@gmail.com>
2022-08-30 18:04:45 +0200

    fix

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comic;
 
 class ComicController extends Controller
 {
@@ -13,7 +14,13 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+        $comics = Comic::all(); 
+
+        $data = [
+            'comics' => $comics
+        ];
+
+       return view('comics.index', $data);
     }
 
     /**
---
+++ resources/views/layouts/comics/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('main_content')
+    <h1>Scopri i nostri nostri fumetti</h1>
+
+    {{dd($comics)}}
+@endsection
