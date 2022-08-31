1cfbd47daf7ab5868ca6e3d61dc855de0e54a584
jacoposergio <djago32@gmail.com>
2022-08-31 15:35:10 +0200

    old

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -19,42 +19,42 @@
 
     <div>
         <label for="title">Title</label>
-        <input type="text" id="title" name="title">
+        <input type="text" id="title" name="title" value="{{old('title')}}>
       </div>
       <br>
   <div>
     <br>
     <div>
         <label for="description">Description</label>
-        <textarea id="description" name="description" cols="30" rows="10" ></textarea>
+        <textarea id="description" name="description" cols="30" rows="10">{{old('description')}}</textarea>
       </div>
       <br>
   <div>
     <br>
     <label for="thumb">Url immagine</label>
-    <input type="text" id="thumb" name="thumb">
+    <input type="text" id="thumb" name="thumb" value="{{old('thumb')}}>
   </div>
 <br>
   <div>
     <label for="price">Price</label>
-    <input type="text" id="price" name="price">
+    <input type="text" id="price" name="price" value="{{old('price')}}>
   </div>
   <br>
 
   <div>
     <label for="series">Series</label>
-    <input type="text" id="series" name="series">
+    <input type="text" id="series" name="series" value="{{old('series')}}>
   </div>
 
   <br>
   <div>
     <label for="sale_date">Sala Date</label>
-    <input type="text" id="sale_date" name="sale_date">
+    <input type="text" id="sale_date" name="sale_date" value="{{old('sale_date')}}>
   </div>
   <br>
   <div>
     <label for="type">Type</label>
-    <input type="text" id="type" name="type">
+    <input type="text" id="type" name="type" value="{{old('type')}}>
   </div>
   <br>
 
